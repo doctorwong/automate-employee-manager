dc9b75d776f2d89174377afd65b7ecdb6f996e01
Wilson <williewonga@gmail.com>
2018-01-30 13:17:55 -0600

    1-30

---
+++ nightwatch/supporting/data.js
@@ -0,0 +1,17 @@
+module.exports = {
+    employees: [{name: "Bernice Ortiz", phone: '4824931093', title: 'CEO', id: '1'},
+        {name: "Marnie Barnett", phone: '3094812387', title: 'CTO', id: '2'},
+        {name: "Phillip Weaver", phone: '7459831843', title: 'Manager', id: '3'},
+        {name: "Teresa Osborne", phone: '3841238745', title: 'Director of Engineering', id: '4'},
+        {name: "Dollie Berry", phone: '4873459812', title: 'Front-End Developer', id: '5'},
+        {name: "Harriett Williamson", phone: '6571249801', title: 'Front-End Developer', id: '6'},
+        {name: "Ruby Estrada", phone: '5740923478', title: 'Back-End Developer', id: '7'},
+        {name: "Lou White", phone: '8727813498', title: 'Full-Stack Developer', id: '8'},
+        {name: "Eve Sparks", phone: '8734567810', title: 'Product Manager', id: '9'},
+        {name: "Lois Brewer", phone: '8749823456', title: 'Sales Manager', id: '10'}],
+    validData: ['foo', '1231234567', 'foobar'],
+    tooLong: 'supercalifragilisticexpialidocious',
+    nameError:'The name field must be between 1 and 30 characters long.',
+    phoneError: 'T he phone number must be 10 digits long.',
+    titleError: 'The title field must be between 1 and 30 characters long.'
+}
---
+++ nightwatch/supporting/selectors.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    inputs: ['[name = "nameEntry"]', '[name = "phoneEntry"]', '[name = "titleEntry"]'],
+    employeeID: '#employeeID',
+    saveButton: 'button[name= "save"]',
+    cancelButton: 'button[name = "cancel"]',
+    errorCard: 'div[class = "errorCard"]',
+    employeeProfiles: ['[name="employee1"]',
+    '[name="employee2"]',
+    '[name="employee3"]',
+    '[name="employee4"]',
+    '[name="employee5"]',
+    '[name="employee6"]',
+    '[name="employee7"]',
+    '[name="employee8"]',
+    '[name="employee9"]',
+    '[name="employee10"]',]
+}
